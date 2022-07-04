104f323ca37622c55f51ce7c9cb1966427cc55a1
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-07-04 18:16:34 +0300

    Add CRUD methods for post model

---
+++ src/models/post.model.ts
@@ -0,0 +1,37 @@
+import db from "../db";
+
+const TABLE = "posts";
+
+const create = async (content: string) => {
+  return await db.query(
+    `INSERT INTO ${TABLE} (content) VALUES ($1) RETURNING *`,
+    [content]
+  );
+};
+
+const all = async (limit = 100) => {
+  return await db.query(`SELECT * FROM ${TABLE} LIMIT $1`, [limit]);
+};
+
+const get = async (id: number) => {
+  return await db.query(`SELECT * FROM ${TABLE} WHERE id = $1 LIMIT 1`, [id]);
+};
+
+const update = async (id: number, content: string) => {
+  return await db.query(
+    `UPDATE ${TABLE} SET content = $1, updated_at = NOW() WHERE id = $2 RETURNING *`,
+    [content, id]
+  );
+};
+
+const destroy = async (id: number) => {
+  return await db.query(`DELETE FROM ${TABLE} WHERE id = $1`, [id]);
+};
+
+export default {
+  create,
+  all,
+  get,
+  update,
+  destroy,
+};

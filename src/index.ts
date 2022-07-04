065d49e129ddf59d3336d9602d44a2b8f2da0966
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-07-04 22:47:49 +0300

    Remove non executed event

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,4 @@ app.get(/^\/posts\/(?<id>\d+)$/, controllers.posts.show);
 app.patch(/^\/posts\/(?<id>\d+)$/, controllers.posts.update);
 app.delete(/^\/posts\/(?<id>\d+)$/, controllers.posts.destroy);
 
-app.server.on("end", () => client.end());
-
 app.server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

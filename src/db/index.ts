cd2f2e20d62ac9b7f9e670650d4a862d065d677b
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-07-05 14:00:48 +0300

    Use `DATABASE_URL` if available

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
 import { Client } from "pg";
 
-const client = new Client();
+const client = new Client(process.env.DATABASE_URL || {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+});
 
 export default client;
